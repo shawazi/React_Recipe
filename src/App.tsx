4951aab3e0d76d314eea2e1a7e6c890c99aeaa44
“shawazi” <shawazisonfire@gmail.com>
2023-07-29 06:27:51 -0400

    switched to hashrouter for gh-pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Details from "./pages/details/Details";
